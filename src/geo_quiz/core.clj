9421279d9131f5bc4541626d19ae93221a89e381
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-02-02 21:19:18 +0100

    Commented functions

--- src/geo_quiz/core.clj
+++ src/geo_quiz/core.clj
@@ -5,23 +5,33 @@
                {:country "Germany" :capital "Berlin"}
                {:country "United Kingdom" :capital  "London"}])
 
-(defn ask-capital [question]
+(defn ask-capital
+  "Ask the :capital of a :country using the given map and returns true
+  if the user enters the correct answer. False otherwise."
+  [question]
   (do
     (println (format "Enter the capital of %s: " (question :country)))
     (= (read-line) (question :capital))))
 
-(defn calculate-points [points correct-answer]
+(defn calculate-points
+  "Calculate the new amount of points given a new correct or wrong
+  answer. +/- 1 respectively."
+  [points correct-answer]
   (if correct-answer
     (inc points)
     (dec points)))
 
 (defn ask-capitals [questions]
+  "Ask the capitals and calculate the amount of points totalized
+  given the user's answers."
   (loop [qs questions points 0]
     (if (empty? qs)
       points
       (recur (rest qs) (calculate-points points (ask-capital (first qs)))))))
 
-(defn quiz [n questions]
+(defn quiz
+  "Ask n random capitals from the list."
+  [n questions]
   (ask-capitals (take n (shuffle capitals))))
     
 (println "RESULT: " (quiz 2 capitals))
