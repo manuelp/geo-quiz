5aafe9dd9785c519fc5ae76e411fb74a1da3b784
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-02-06 18:35:23 +0100

    Even shorter

--- src/geo_quiz/core.clj
+++ src/geo_quiz/core.clj
@@ -1,18 +1,17 @@
 (ns geo-quiz.core)
 
-(def capitals [{:country "Italy" :capital "Rome"}
-               {:country "France" :capital "Paris"}
-               {:country "Germany" :capital "Berlin"}
-               {:country "United Kingdom" :capital  "London"}])
-
 (defn quiz
   "Ask n random capitals from the list."
   [n questions]
-  (letfn [(ask-capital [question]
-            (do
-              (println (format "Enter the capital of %s: " (:country question)))
-              (if (= (read-line) (:capital question)) 1 -1)))
-          (ask-capitals [questions] (reduce + (map ask-capital questions)))]
-    (ask-capitals (take n (shuffle capitals)))))
+  (let [capitals [{:country "Italy" :capital "Rome"}
+                  {:country "France" :capital "Paris"}
+                  {:country "Germany" :capital "Berlin"}
+                  {:country "United Kingdom" :capital  "London"}]]
+    (letfn [(ask-capital [question]
+              (do
+                (println (format "Enter the capital of %s: " (:country question)))
+                (if (= (read-line) (:capital question)) 1 -1)))
+            (ask-capitals [questions] (reduce + (map ask-capital questions)))]
+      (ask-capitals (take n (shuffle capitals))))))
     
 (println "RESULT: " (quiz 2 capitals))
