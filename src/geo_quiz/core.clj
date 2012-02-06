72ca11164be8502ea709e03f312916331cbcf3f2
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-02-06 18:15:58 +0100

    Incorporated feedback from c.l.c

--- src/geo_quiz/core.clj
+++ src/geo_quiz/core.clj
@@ -5,23 +5,14 @@
                {:country "Germany" :capital "Berlin"}
                {:country "United Kingdom" :capital  "London"}])
 
-(defn ask-capital
-  "Ask the :capital of a :country using the given map and returns 1 if
-  the user answered correctly, -1 otherwise."
-  [question]
-  (do
-    (println (format "Enter the capital of %s: " (question :country)))
-    (if (= (read-line) (question :capital)) 1 -1)))
-
-(defn ask-capitals
-  "Ask to the user the capital of the country coll and returns the sum of
-  the points for every answer."
-  [questions]
-  (reduce + (map ask-capital questions)))
-
 (defn quiz
   "Ask n random capitals from the list."
   [n questions]
-  (ask-capitals (take n (shuffle capitals))))
+  (letfn [(ask-capital [question]
+            (do
+              (println (format "Enter the capital of %s: " (:country question)))
+              (if (= (read-line) (:capital question)) 1 -1)))
+          (ask-capitals [questions] (reduce + (map ask-capital questions)))]
+    (ask-capitals (take n (shuffle capitals)))))
     
 (println "RESULT: " (quiz 2 capitals))
