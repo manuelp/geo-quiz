256400ef7438127778cc9eb6faefebaffec57fd5
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-02-07 21:45:42 +0100

    Used destructuring

--- src/geo_quiz/core.clj
+++ src/geo_quiz/core.clj
@@ -2,16 +2,16 @@
 
 (defn quiz
   "Ask n random capitals from the list."
-  [n questions]
+  [n]
   (let [capitals [{:country "Italy" :capital "Rome"}
                   {:country "France" :capital "Paris"}
                   {:country "Germany" :capital "Berlin"}
                   {:country "United Kingdom" :capital  "London"}]]
-    (letfn [(ask-capital [question]
+    (letfn [(ask-capital [{:keys [country capital]}]
               (do
-                (println (format "Enter the capital of %s: " (:country question)))
-                (if (= (read-line) (:capital question)) 1 -1)))
+                (println (format "Enter the capital of %s: " country))
+                (if (= (read-line) capital) 1 -1)))
             (ask-capitals [questions] (reduce + (map ask-capital questions)))]
       (ask-capitals (take n (shuffle capitals))))))
     
-(println "RESULT: " (quiz 2 capitals))
+(println "RESULT: " (quiz 2))
